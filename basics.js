9f7ee5f4639400cbabf24dd1f480ffb7bb2e2272
AHONYO-ISRAEL <124524199+AHONYO-ISRAEL@users.noreply.github.com>
2023-03-26 11:57:20 -1200

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,7 @@
+const hamburgerButton = document.querySelector(".nav-toggle")
+const navigation = document.querySelector("nav")
+hamburgerButton.addEventListener("click", toggleNav)
+function toggleNav() {
+    hamburgerButton.classList.toggle("active")
+    navigation.classList.toggle("active")
+}
